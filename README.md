3718fd31bd908228c5b36eeb5af7d3bebd4d5eaa
root <root@localhost.localdomain>
2018-11-19 17:14:19 +0100

    Added cat mode

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ For this "themer" multiple files were taken from other users.
 
 - [Terminal Changer](https://askubuntu.com/a/676452)
 - [Windows Manager Themes](https://github.com/addy-dclxvi/Xfwm4-Theme-Collections)
+- [Windows Manager Dot Theme](https://github.com/rafacuevas3/dots-theme)
 
 ### Usage
 
@@ -28,3 +29,14 @@ Apply dark colored theme
 ```bash
 ./changeTheme.sh dark
 ```
+
+Apply CAT colored theme (it's also dark)
+
+```bash
+./changeTheme.sh cat
+```
+
+#### Notes
+
+In case the windows manager does not support resizing on Windows, you can use `alt` and `right click` to manually resize the window to the size you prefer.
+
--- changeTheme.sh
+++ changeTheme.sh
@@ -1,11 +1,12 @@
-# Update the OS
-#apt update -y && apt upgrade -y
+# Developed for fun and profit with love and love 
 
 if [ "$1" = "setup" ]
 then
 	echo 'Downloading themes'
 	git clone https://github.com/addy-dclxvi/Xfwm4-Theme-Collections ~/.themes
-	
+	git clone https://github.com/rafacuevas3/dots-theme.git ~/.themes/dot-theme
+	cp ~/.themes/dot-themes/Dots* -r ~/.themes	
+
 	# Update VIM configuration
 	echo 'Updating vimrc...' 
 	echo "set number" > ~/.vimrc
@@ -15,7 +16,8 @@ then
 	# Download Images
 	wget https://i.imgur.com/aSPQKgD.jpg -O ~/Downloads/dark.jpg
 	wget https://i.imgur.com/GYHtxD0.png -O ~/Downloads/light.jpg
-	
+	wget https://i.imgur.com/mlvyx89.jpg -O ~/Downloads/cat1.jpg	
+
 	# Install Icons
 	apt install numix-icon-theme
 	
@@ -93,6 +95,16 @@ then
 	xfconf-query -c xfwm4 -p /general/theme -s "Rin"
 	xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s ~/Downloads/light.jpg	
 	xfconf-query -c xsettings -p /Net/IconThemeName -s "Adwaita"
+	~/change-term.sh eighties 	
+fi
+
+if [ "$1" = "cat" ]
+then
+	# Theme manager and then Window Manager
+	xfconf-query -c xsettings -p /Net/ThemeName -s "Kali-X-Dark"	
+	xfconf-query -c xfwm4 -p /general/theme -s "Dots Black"
+	xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s ~/Downloads/cat1.jpg	
+	xfconf-query -c xsettings -p /Net/IconThemeName -s "Numix"
 	~/change-term.sh eighties 	
 fi
 
